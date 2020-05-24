1e311c0915e8ce3bc803d0882f3234a002b14316
LowAndroider <1317450073@qq.com>
2020-05-24 16:55:43 +0800

    公共模块提取

---
+++ cloud-api-commons/src/main/java/com/springcloud/demo2020/config/ApplicationContextConfig.java
@@ -0,0 +1,17 @@
+package com.springcloud.demo2020.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author DJH
+ */
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
